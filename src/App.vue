360154f1bc57a4c583783ee2f620f20ad7b6b2d7
Viktor Filippov <jackal3166@gmail.com>
2024-02-18 23:56:44 +0200

    Add transactions

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <script setup>
+	import { ref } from 'vue';
 	import Header from './components/Header.vue';
 	import Balance from './components/Balance.vue';
 	import IncomeExpenses from './components/IncomeExpenses.vue';
 	import TransactionsList from './components/TransactionsList.vue';
 	import AddTransactions from './components/AddTransactions.vue';
+
+	const transactions = ref([
+		{ id: 1, text: 'Flower', amount: -20.99 },
+		{ id: 2, text: 'Salary', amount: 300.67 },
+		{ id: 3, text: 'Book', amount: -10 },
+		{ id: 4, text: 'Camera', amount: 150 },
+	]);
 </script>
 
 <template>
-	<main class="flex items-center justify-center min-h-screen text-slate-400 bg-slate-900">
+	<main
+		class="flex items-center justify-center min-h-screen text-slate-400 bg-slate-900"
+	>
 		<div
 			class="flex flex-col w-4/5 max-w-xl gap-5 p-10 mx-auto my-10 text-center rounded shadow-lg shadow-slate-400"
 		>
 			<Header />
 			<Balance />
 			<IncomeExpenses />
-			<TransactionsList />
+			<TransactionsList :transactions="transactions" />
 			<AddTransactions />
 		</div>
 	</main>
--- src/components/TransactionsList.vue
+++ src/components/TransactionsList.vue
@@ -1,17 +1,30 @@
-<script setup></script>
+<script setup>
+	defineProps({
+		transactions: {
+			type: Array,
+			required: true,
+		}
+	})
+</script>
 
 <template>
 	<h3 class="text-xl font-bold">Transactions</h3>
-	<ul id="list" class='flex flex-col gap-3'>
-		<li class="relative flex justify-between px-5 py-1 rounded shadow group shadow-red-500">
-			Cash <span>-€400.00</span><button class="absolute text-red-500 transition-all duration-1000 opacity-0 -left-5 group-hover:opacity-100">&#10006;</button>
-		</li>
-		<li class="relative flex justify-between px-5 py-1 rounded shadow group shadow-red-500">
-			Cash <span>-€40.00</span><button class="absolute text-red-500 transition-all duration-1000 opacity-0 -left-5 group-hover:opacity-100">&#10006;</button>
-		</li>
-		<li class="relative flex justify-between px-5 py-1 rounded shadow group shadow-green-500">
-			Paycheck <span>+€400.00</span
-			><button class="absolute text-red-500 transition-all duration-1000 opacity-0 -left-5 group-hover:opacity-100">&#10006;</button>
+	<ul id="list" class="flex flex-col gap-3">
+		<li
+			v-for="transaction in transactions"
+			:key="transaction.id"
+			:class="
+				transaction.amount > 0 ? 'shadow-green-500' : 'shadow-red-500'
+			"
+			class="relative flex justify-between px-5 py-1 rounded shadow group"
+		>
+			{{ transaction.text }}
+			<span>{{ transaction.amount.toFixed(2) }}€</span
+			><button
+				class="absolute text-red-500 transition-all duration-1000 opacity-0 -left-5 group-hover:opacity-100"
+			>
+				&#10006;
+			</button>
 		</li>
 	</ul>
 </template>
