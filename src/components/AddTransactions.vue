5388d421706336aa5f0fc7d3e36f8eb6fc36fef2
Viktor Filippov <jackal3166@gmail.com>
2024-02-18 23:14:42 +0200

    Add styles

--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -3,17 +3,17 @@
 <template>
 	<h3 class="text-xl font-bold">Add New Transactions</h3>
 	<form id="form">
-		<div class="-mt-5">
-			<label for="text">Text</label>
+		<div>
+			<label for="text" class="text-xl">Text</label>
 			<input
 				type="text"
 				id="text"
 				placeholder="Enter text..."
-				class="rounded outline-none focus:shadow-violet-500 focus:shadow-inner"
+				class="w-full px-5 py-2 my-5 rounded outline-none focus:shadow-violet-500 focus:shadow-inner"
 			/>
 		</div>
 		<div>
-			<label for="amount"
+			<label for="amount" class="text-xl"
 				>Amount <br />
 				(negative - expense, positive - income)</label
 			>
@@ -21,10 +21,10 @@
 				type="number"
 				id="amount"
 				placeholder="Enter amount..."
-				class="rounded outline-none focus:shadow-violet-500 focus:shadow-inner"
+				class="w-full px-5 py-2 mt-5 rounded outline-none focus:shadow-violet-500 focus:shadow-inner"
 			/>
 			<button
-				class="w-full px-5 py-2.5 mt-8 duration-300 rounded bg-violet-500 text-slate-200 hover:bg-violet-600"
+				class="w-full px-5 py-2 mt-8 duration-300 rounded bg-violet-500 text-slate-200 hover:bg-violet-600"
 			>
 				Add Transaction
 			</button>
--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 
 <template>
-	<h4 class="text-xl font-bold uppercase">Your Balance</h4>
+	<h4 class="text-xl font-bold underline uppercase underline-offset-4">Your Balance</h4>
 	<h1 class="text-3xl font-semibold">€0.00</h1>
 </template>
